7adec582a0762d0f60bfef081917d4752b27825f
Afikur Rahman <afikur.khan@dsinnovators.com>
2019-10-10 16:00:48 +0600

    add css, sass, image support

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 module.exports = {
   entry: './src/app.js',
   devServer: {
@@ -10,6 +10,7 @@ module.exports = {
     hot: true
   },
   plugins: [
+    new CopyWebpackPlugin([{ from: 'src/assets', to: 'assets' }]),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: './src/index.html'
@@ -19,5 +20,31 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: {
+          loader: 'url-loader',
+        }
+      }
+    ]
   }
 };
